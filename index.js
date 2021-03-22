42ddce87ae2780ab096699b2f798d75c25ab743b
Noah <65922318+N0ahMC@users.noreply.github.com>
2021-03-22 10:56:19 -0500

    ✨ Add ShareX api

--- index.js
+++ index.js
@@ -1,14 +1,44 @@
 const express = require('express')
+require('dotenv').config()
 const app = express()
 const fs = require('fs')
+const fileUpload = require('express-fileupload');
 
 app.set('view engine', 'ejs')
 
 app.use('/assets', express.static('assets'))
 app.use('/image', express.static('images'))
+app.use(fileUpload())
 const types = ['', '.png', '.jpg', '.jpeg']
 let Rpath = ''
 let Rtype = ''
+app.post("/upload", (req, res) => {
+    try {
+      if (req.headers.key !== process.env.TOKEN) {
+        return res.status(403).send({ status: 403, message: "Invalid token" });
+      } else {
+        if (!req.files) {
+          res.status(404).send({
+            status: 404,
+            message: "No file uploaded",
+          });
+        } else {
+          let avatar = req.files.sharex;
+          avatar.mv("./images/" + avatar.name);
+          //send response
+          res.send({
+            status: 200,
+            message: "File just got uploaded!",
+            url: avatar.name,
+          });
+        }
+      }
+    } catch (err) {
+        console.log(err)
+      res.status(500).send(err);
+    }
+  });
+  
 app.get('/:image', (req, res) => {
     types.forEach(i => {
         if (fs.existsSync(`images/${req.path.slice(1)}${i}`)) {
