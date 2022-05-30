6c9e13b32c0303b62512b8db65763ae32db83560
Restyled.io <commits@restyled.io>
2022-05-30 15:46:36 +0000

    Restyled by prettier

--- index.js
+++ index.js
@@ -7,47 +7,58 @@ const path = require("path");
 
 if (!process.env.PORT && !fs.existsSync("./.env")) {
   console.error(
-      "\x1b[31m", "[ERROR]", "\x1b[0m",
-      "The .env file could not be found.\nPlease check README.md for more information");
+    "\x1b[31m",
+    "[ERROR]",
+    "\x1b[0m",
+    "The .env file could not be found.\nPlease check README.md for more information"
+  );
   process.exit(1);
 }
 if (!process.env.KEY || process.env.KEY === "CHANGE_ME") {
   console.warn(
-      "\x1b[33m", "[WARNING]", "\x1b[0m",
-      "Your key has not been set.\nPlease check your .env configuration.");
+    "\x1b[33m",
+    "[WARNING]",
+    "\x1b[0m",
+    "Your key has not been set.\nPlease check your .env configuration."
+  );
 }
-if (process.env.LOGO_FILE_NAME &&
-    !fs.existsSync(`assets/images/${process.env.LOGO_FILE_NAME}`)) {
+if (
+  process.env.LOGO_FILE_NAME &&
+  !fs.existsSync(`assets/images/${process.env.LOGO_FILE_NAME}`)
+) {
   console.warn(
-      "\x1b[33m", "[WARNING]", "\x1b[0m",
-      "The logo file could not be found.\nPlease check your .env configuration.");
+    "\x1b[33m",
+    "[WARNING]",
+    "\x1b[0m",
+    "The logo file could not be found.\nPlease check your .env configuration."
+  );
 }
 
 fastify.register(require("fastify-helmet"), {
-  contentSecurityPolicy : false,
+  contentSecurityPolicy: false,
 });
 
 fastify.register(require("point-of-view"), {
-  engine : {
-    ejs : require("ejs"),
+  engine: {
+    ejs: require("ejs"),
   },
 });
 
 fastify.register(fastifyStatic, {
-  root : path.join(__dirname, "assets"),
-  prefix : "/assets",
+  root: path.join(__dirname, "assets"),
+  prefix: "/assets",
 });
 
 fastify.register(require("fastify-formbody"));
 
 fastify.register(fastifyStatic, {
-  root : path.join(__dirname, "images"),
-  prefix : "/image",
-  decorateReply : false,
+  root: path.join(__dirname, "images"),
+  prefix: "/image",
+  decorateReply: false,
 });
 
 fastify.register(require("fastify-file-upload"));
-const types = [ "", ".png", ".jpg", ".jpeg" ];
+const types = ["", ".png", ".jpg", ".jpeg"];
 let Fpath = "";
 let Ftype = "";
 let Fsize = "";
@@ -55,24 +66,25 @@ let Fdate = "";
 fastify.post("/", async (req, res) => {
   try {
     if (req.headers.key !== process.env.KEY) {
-      return res.status(403).send({status : 403, message : "Invalid token"});
+      return res.status(403).send({ status: 403, message: "Invalid token" });
     }
     const file = req.body?.file;
 
     if (!file) {
       return res.status(404).send({
-        status : 404,
-        message : "No file uploaded",
+        status: 404,
+        message: "No file uploaded",
       });
     }
-    const safeSuffix =
-        path.normalize(file.name).replace(/^(\.\.(\/|\\|$))+/, "");
+    const safeSuffix = path
+      .normalize(file.name)
+      .replace(/^(\.\.(\/|\\|$))+/, "");
     const safeJoin = path.join("./images/", safeSuffix);
     file.mv(safeJoin);
     res.send({
-      status : 200,
-      message : "File just got uploaded!",
-      url : safeSuffix,
+      status: 200,
+      message: "File just got uploaded!",
+      url: safeSuffix,
     });
     if (process.env.ADVANCED_LOGGING === "true") {
       console.log(`File ${safeSuffix} uploaded!`);
@@ -86,54 +98,59 @@ fastify.get("/robots.txt", (req, res) => {
   res.type("text/plain");
   res.send("User-agent: *\nAllow: /$\nDisallow: /");
 });
-fastify.get("/admin", (req, res) => { res.view("views/login"); });
+fastify.get("/admin", (req, res) => {
+  res.view("views/login");
+});
 fastify.post("/admin", (req, res) => {
   if (req.body.password === process.env.KEY) {
     let images = [];
     fs.readdirSync("images/").forEach((image) => {
       images.push({
-        fullPath : image,
-        size : fs.statSync(`images/${image}`).size / 1000,
-        date : fs.statSync(`images/${image}`).mtime.toLocaleDateString("en-US"),
+        fullPath: image,
+        size: fs.statSync(`images/${image}`).size / 1000,
+        date: fs.statSync(`images/${image}`).mtime.toLocaleDateString("en-US"),
       });
     });
     images.sort((a, b) => a - b);
     const totalSize = images.reduce(
-        (previousValue, initialValue) => previousValue + initialValue.size, 0);
+      (previousValue, initialValue) => previousValue + initialValue.size,
+      0
+    );
     const statistics = {
-      totalNb : images.length,
-      mostRecentUpload : images[0].date,
-      totalSize : totalSize > 1000
-                      ? `${Math.round((totalSize * 100) / 1000) / 100} MB`
-                      : `${Math.round(totalSize * 100) / 100} KB`,
+      totalNb: images.length,
+      mostRecentUpload: images[0].date,
+      totalSize:
+        totalSize > 1000
+          ? `${Math.round((totalSize * 100) / 1000) / 100} MB`
+          : `${Math.round(totalSize * 100) / 100} KB`,
     };
-    images = images.map(
-        (image) =>
-            image.size > 1000
-                ? {
-                    ...image,
-                    size : `${Math.round((image.size * 100) / 1000) / 100} MB`,
-                  }
-                : {
-                    ...image,
-                    size : `${Math.round(image.size * 100) / 100} KB`
-                  });
+    images = images.map((image) =>
+      image.size > 1000
+        ? {
+            ...image,
+            size: `${Math.round((image.size * 100) / 1000) / 100} MB`,
+          }
+        : {
+            ...image,
+            size: `${Math.round(image.size * 100) / 100} KB`,
+          }
+    );
     console.log("Login successful!");
     res.view("views/admin", {
       statistics,
       images,
-      fileExists : fs.existsSync,
+      fileExists: fs.existsSync,
     });
   } else {
     console.log("Failed to login!");
     res.view("views/404", {
-      path : req.url,
+      path: req.url,
     });
   }
 });
 fastify.get("/", (req, res) => {
   res.view("views/index", {
-    path : req.url,
+    path: req.url,
   });
 });
 fastify.get("/:image", (req, res) => {
@@ -144,28 +161,31 @@ fastify.get("/:image", (req, res) => {
       const size = fs.statSync(`images/${req.url}${i}`).size / 1000;
       Fpath = req.url;
       Ftype = i;
-      Fsize = size > 1000 ? `${Math.round((size * 100) / 1000) / 100} MB`
-                          : `${Math.round(size * 100) / 100} KB`;
-      Fdate = fs.statSync(`images/${req.url}${i}`)
-                  .mtime.toLocaleDateString("en-US");
+      Fsize =
+        size > 1000
+          ? `${Math.round((size * 100) / 1000) / 100} MB`
+          : `${Math.round(size * 100) / 100} KB`;
+      Fdate = fs
+        .statSync(`images/${req.url}${i}`)
+        .mtime.toLocaleDateString("en-US");
     }
   });
   const fullPath = Fpath + Ftype;
   if (fullPath !== "" && fs.existsSync(`images/${fullPath}`)) {
     res.view("views/image", {
-      path : Fpath,
-      type : Ftype,
+      path: Fpath,
+      type: Ftype,
       fullPath,
-      size : Fsize,
-      date : Fdate,
-      fileExists : fs.existsSync,
+      size: Fsize,
+      date: Fdate,
+      fileExists: fs.existsSync,
     });
     if (process.env.ADVANCED_LOGGING === "true" && fullPath) {
       console.log(`File ${fullPath} viewed!`);
     }
   } else {
     res.view("views/404", {
-      path : req.url,
+      path: req.url,
     });
   }
 });
@@ -174,7 +194,10 @@ fastify.listen(process.env.PORT, (err) => {
   if (err) {
     throw new Error(err);
   }
-  console.log("\x1b[32m", "[READY]", "\x1b[0m",
-              `ImageWebServer running on ${process.env.DOMAIN}, using port ${
-                  process.env.PORT}!`);
+  console.log(
+    "\x1b[32m",
+    "[READY]",
+    "\x1b[0m",
+    `ImageWebServer running on ${process.env.DOMAIN}, using port ${process.env.PORT}!`
+  );
 });
